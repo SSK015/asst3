309b7bc79ea40335b11dd9692aeb3ee833dccaca
SSK015 <18252677381@163.com>
2023-03-16 17:54:45 +0800

    speedup1

--- src/parallel-simulator.cpp
+++ src/parallel-simulator.cpp
@@ -17,6 +17,42 @@ public:
   // particles. You do not have to preserve this function type.
   std::unique_ptr<QuadTreeNode> buildQuadTree(std::vector<Particle> &particles,
                                               Vec2 bmin, Vec2 bmax) {
+    if ((int)particles.size() <= QuadTreeLeafSize) {
+//        auto Node = new QuadTreeNode;
+        std::unique_ptr<QuadTreeNode> Node(new QuadTreeNode); 
+ 
+        Node->isLeaf = 1;
+        Node->particles = particles;
+        return Node;
+    } else {
+      std::unique_ptr<QuadTreeNode> result(new QuadTreeNode);
+//      auto result = new QuadTreeLeafSize;
+      result->isLeaf = 0;
+      result->particles = particles;
+      auto pivot = (bmin + bmax) * 0.5f;
+      Vec2 size = (bmax - bmin) * 0.5f;
+    #pragma omp parallel for
+    for (int i = 0; i < 4; i++) {
+      Vec2 childBMin;
+      childBMin.x = (i & 1) ? pivot.x : bmin.x;
+      childBMin.y = ((i >> 1) & 1) ? pivot.y : bmin.y;
+      Vec2 childBMax = childBMin + size;
+      std::vector<Particle> newpar;
+      
+      for (int i = 0; i < (int)particles.size(); i++) {
+        
+        auto pi = particles[i];
+      
+        if (pi.position.x < childBMax.x && pi.position.y < childBMax.y && pi.position.x >= childBMin.x && pi.position.y >= childBMin.y) {
+          newpar.push_back(pi);
+        }
+      }
+      result->children[i] = buildQuadTree(newpar, childBMin, childBMax);
+  }
+      // <assign particles to appropriate children of result>
+      
+      return result;
+    }
     return nullptr;
   }
 
@@ -55,7 +91,32 @@ public:
                             std::vector<Particle> &newParticles,
                             StepParameters params) override {
     // TODO: implement parallel version of quad-tree accelerated n-body
+  
     // simulation here, using quadTree as acceleration structure
+    #pragma omp parallel for
+    for (int i = 0; i < (int)particles.size(); i++) {
+        
+      
+      auto pi = particles[i];
+      std::vector<Particle> newpar;
+
+// getParticlesImpl(std::vector<Particle> &particles, QuadTreeNode *node,
+//                      Vec2 bmin, Vec2 bmax, Vec2 position, float radius) {
+      //#pragma omp for schedule(dynamic)
+      accel->getParticles(newpar, pi.position, params.cullRadius); 
+      
+      Vec2 force = Vec2(0.0f, 0.0f);
+      // accumulate attractive forces to apply to particle i
+//      #pragma omp parallel for
+//      #pragma omp for schedule(dynamic)
+      for (size_t j = 0; j < newpar.size(); j++) {
+      //  if (j == i)
+      //    continue;
+          force += computeForce(pi, newpar[j], params.cullRadius);
+      }
+      // update particle state using the computed force
+      newParticles[i] = updateParticle(pi, force, params.deltaTime);
+    }
   }
 };
 
@@ -63,3 +124,4 @@ public:
 std::unique_ptr<INBodySimulator> createParallelNBodySimulator() {
   return std::make_unique<ParallelNBodySimulator>();
 }
+
